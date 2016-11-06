1dcdee256261b453c676e2d71086ce078eaf5d12
ibooio <iboosocial@gmail.com>
2016-11-06 19:10:48 -0300

    visual changes contact section

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title></title>
+    <title>Exposolidaria 2016 - Una forma de hacer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <meta name="keywords" content="">
-    <meta name="description" content="">
+    <meta name="description" content="Exposolidaria 2016 - Una forma de hacer">
     <meta name="author" content="Indary">
     <meta name="theme-color" content="#12356b">
     <!-- TWITTER -->
@@ -20,18 +20,18 @@
     <meta name="twitter:creator" content="@indary">
     <meta name="twitter:title" content="Expo Solidaria">
     <meta name="twitter:description" content="">
-    <meta name="twitter:image:src" content="<?php echo base_url('img/logo.png')?>">
+    <meta name="twitter:image:src" content="<?php echo base_url('img/logo.jpg')?>">
     <!-- FACEBOOK -->
     <meta property="og:site_name" content="Expo Solidaria">
     <meta property="og:url" content="<?php echo base_url()?>">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="Exposolidaria">
+    <meta property="og:title" content="Exposolidaria - Una forma de hacer">
     <meta property="og:description" content="">
-    <meta property="og:image" content="<?php echo base_url('img/logo.png')?>">
+    <meta property="og:image" content="<?php echo base_url('img/logo.jpg')?>">
     <!-- G++ -->
     <meta itemprop="name" content="Exposolidaria">
     <meta itemprop="description" content="">
-    <meta itemprop="image" content="<?php echo base_url('img/logo.png')?>">
+    <meta itemprop="image" content="<?php echo base_url('img/logo.jpg')?>">
     <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
     <!--Import Google Icon Font-->
      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -41,7 +41,6 @@
     <link rel="stylesheet" href="libs/indary/css/indary.css" type="text/css">
     <script src="libs/jquery/jquery-1.11.3.min.js"></script>
     <script src="libs/materialize/js/materialize.min.js"></script>
-    <script src="libs/wow/wow.min.js"></script>
     <script src="js/global.js"></script>
   </head>
   <body>
@@ -64,6 +63,7 @@
       <div class="override">
         <div class="container center">
           <h1 class="">#exposolidaria</h1>
+          <h3 class="">UNA FORMA DE HACER</h3>
           <h4 class="expo-date">04 DE DICIEMBRE - COMODORO RIVADAVIA</h4>
           <div id="timer"></div>
         </div>
@@ -72,50 +72,78 @@
     <div id="full-wrapper">
       <section id="about" class="">
         <div class="container">
-          <h5 class="title">ACERCA DE</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a orci eu orci lacinia finibus. Aenean et mi eros. Nam in sodales dolor. Integer in pretium eros, ac finibus magna. Suspendisse potenti. Morbi sodales lorem eu lacus rutrum, a imperdiet orci consectetur. Vestibulum congue orci nisl, ut aliquam metus mattis ut. Nulla nibh sapien, varius ac gravida et, varius ut arcu. Aliquam vel rhoncus lectus. Proin dolor dui, placerat in augue vel, vestibulum fringilla nisl.</p>
           <p>Nam quis augue posuere ante tristique semper. Mauris eu tellus dui. Nulla rutrum eros eu leo facilisis malesuada. Duis non luctus lacus. Nunc venenatis faucibus tortor, sit amet faucibus sem malesuada eget. Donec venenatis mauris a sapien rutrum, vel dignissim sapien cursus. Praesent vel tristique eros. Nunc ultricies blandit tellus, ut lacinia massa bibendum a. Integer at magna vitae lorem tincidunt sodales tincidunt vestibulum nibh. Phasellus congue est odio, in luctus purus laoreet et. Suspendisse eu sem libero. Nullam posuere nunc ligula, eget gravida elit venenatis et. Nullam at ultrices quam. Sed interdum, orci et varius tristique, mi urna lacinia felis, ut maximus ligula mauris eget purus. Nunc egestas eget lorem at elementum.</p>
         </div>
       </section>
+      <section id="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2778.4452858240275!2d-67.4747542706295!3d-45.86240013634444!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb8af8c1ef0469af6!2sCentro+de+Informacion+Publica!5e0!3m2!1ses-419!2sar!4v1477261571090" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </section>
       <section id="program" class="">
         <div class="container">
-          <h5 class="title gray">PROGRAMA</h5>
-          <div class="program-box">
-            <div class="program-item">
-              <div class="hour">Hora: 14:00hs</div>
-              <div>Apertura</div>
+          <h5 class="title">PROGRAMA</h5>
+          <div class="row">
+            <div class="col s12 m6 l6">
+              <div class="program-item program-item-blue">
+                <div class="pi-hour">Hora: 14:00hs</div>
+                <div class="pi-title">Apertura</div>
+              </div>
             </div>
-            <div class="program-item">
-              <div class="hour">Hora: 14:30hs</div>
-              <div>Barrileteada</div>
+            <div class="col s12 m6 l6">
+              <div class="program-item program-item-orange">
+                <div class="pi-hour">Hora: 14:30hs</div>
+                <div class="pi-title">Barrileteada</div>
+              </div>
             </div>
-            <div class="program-item">
-              <div class="hour">Hora: 14:30hs</div>
-              <div>Cuenta cuentos</div>
+          </div>
+          <div class="row">
+            <div class="col s12 m6 l6">
+              <div class="program-item program-item-green">
+                <div class="pi-hour">Hora: 14:30hs</div>
+                <div class="pi-title">Cuenta cuentos</div>
+              </div>
             </div>
-            <div class="program-item">
-              <div class="hour">Hora: 16:00hs</div>
-              <div>Charla medio ambiente</div>
+            <div class="col s12 m6 l6">
+              <div class="program-item program-item-pink">
+                <div class="pi-hour">Hora: 16:00hs</div>
+                <div class="pi-title">Charla medio ambiente</div>
+              </div>
             </div>
-            <div class="program-item">
-              <div class="hour">Hora: 17:00hs</div>
-              <div class="name">Charla fortalecimiento a la organizaciones</div>
+          </div>
+          <div class="row">
+            <div class="col s12 m6 l6">
+              <div class="program-item program-item-purple">
+                <div class="pi-hour">Hora: 17:00hs</div>
+                <div class="pi-title">Charla fortalecimiento a la organizaciones</div>
+              </div>
             </div>
-            <div class="program-item">
-              <div class="hour">Hora: 17:40hs</div>
-              <div>Charla motivacional</div>
+            <div class="col s12 m6 l6">
+              <div class="program-item program-item-blue">
+                <div class="pi-hour">Hora: 17:40hs</div>
+                <div class="pi-title">Charla motivacional</div>
+              </div>
             </div>
-            <div class="program-item">
-              <div class="hour">Hora: 18:40hs</div>
-              <div>Tango (Amigos del Tango)</div>
+          </div>
+          <div class="row">
+            <div class="col s12 m6 l6">
+              <div class="program-item program-item-orange">
+                <div class="pi-hour">Hora: 18:40hs</div>
+                <div class="pi-title">Tango (Amigos del Tango)</div>
+              </div>
             </div>
-            <div class="program-item">
-              <div class="hour">Hora: 18:40hs</div>
-              <div>Banda de Rock (El Reflejo)</div>
+            <div class="col s12 m6 l6">
+              <div class="program-item program-item-green">
+                <div class="pi-hour">Hora: 18:40hs</div>
+                <div class="pi-title">Banda de Rock (El Reflejo)</div>
+              </div>
             </div>
-            <div class="program-item">
-              <div class="hour">Hora: 18:40hs</div>
-              <div>Banda de Rock</div>
+          </div>
+          <div class="row">
+            <div class="col s12 m12 l12">
+              <div class="program-item program-item-pink">
+                <div class="pi-hour">Hora: 18:40hs</div>
+                <div class="pi-title">Banda de Rock</div>
+              </div>
             </div>
           </div>
         </div>
@@ -123,7 +151,8 @@
       <div class="quote">
         <div class="quote-content">
           <div class="container center">
-            <p>FRASE</p>
+            <h3><b>CUANDO YO DOY ME DOY A MÍ MISMO</b></h3>
+            <h4>Walt Whitman</h4>
           </div>
         </div>
       </div>
@@ -302,7 +331,7 @@
               </div>
             </div>
             <div class="col s12 m4 l4">
-              <div class="ong-box ong-box-purple">
+              <div class="ong-box ong-box-pink">
                 <div class="ong-box-container">
                   <div class="ong-image">
                     <img src="img/ongs/ceviof.jpg">
@@ -320,7 +349,7 @@
           </div>
           <div class="row">
             <div class="col s12 m4 l4">
-              <div class="ong-box ong-box-orange">
+              <div class="ong-box ong-box-purple">
                 <div class="ong-box-container">
                   <div class="ong-image">
                     <img src="img/ongs/patioabierto.png">
@@ -342,7 +371,7 @@
               </div>
             </div>
             <div class="col s12 m4 l4">
-              <div class="ong-box ong-box-green">
+              <div class="ong-box ong-box-blue">
                 <div class="ong-box-container">
                   <div class="ong-image">
                     <img src="img/ongs/felizentudia.gif">
@@ -362,7 +391,7 @@
               </div>
             </div>
             <div class="col s12 m4 l4">
-              <div class="ong-box ong-box-purple">
+              <div class="ong-box ong-box-orange">
                 <div class="ong-box-container">
                   <div class="ong-image">
                     <img src="img/ongs/fundacioncrecer.jpg">
@@ -386,7 +415,7 @@
           </div>
           <div class="row">
             <div class="col s12 m4 l4">
-              <div class="ong-box ong-box-orange">
+              <div class="ong-box ong-box-green">
                 <div class="ong-box-container">
                   <div class="ong-image">
                     <!--<img src="img/ongs">-->
@@ -404,7 +433,7 @@
               </div>
             </div>
             <div class="col s12 m4 l4">
-              <div class="ong-box ong-box-green">
+              <div class="ong-box ong-box-pink">
                 <div class="ong-box-container">
                   <div class="ong-image">
                     <img src="img/ongs/adc.jpg">
@@ -458,19 +487,19 @@
           </div>
         </div>
       </section>
-      <section id="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2778.4452858240275!2d-67.4747542706295!3d-45.86240013634444!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb8af8c1ef0469af6!2sCentro+de+Informacion+Publica!5e0!3m2!1ses-419!2sar!4v1477261571090" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
-      </section>
     </div>
     <footer id="contact">
       <div class="container">
         <div class="row">
-          <div class="title">DATOS  DE CONTACTO</div>
-          <div class="col l6 m6 s12">
-            <div class="row">
+          <div class="col l12 m12 s12">
+            <p class="center">
+              Por dudas y sugerencias
+            </p>
+            <div class="center">
+              <h4><b>comunicacion@crecerfundacion.org.ar</b></h4>
             </div>
           </div>
-          <div class="col l6 m6 s12">
+          <div class="col l12 m12 s12">
             <ul id="contact-list">
               <li></li>
             </ul>
